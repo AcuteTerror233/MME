b39008c8ea899ce5601424712b995464bf0138ad
server <551775262@qq.com>
2025-05-30 18:39:44 +0800

    5.30.1

--- src/main/java/com/acuteterror233/mite/item/items.java
+++ src/main/java/com/acuteterror233/mite/item/items.java
@@ -13,28 +13,29 @@ import net.minecraft.util.Identifier;
 import java.util.function.Function;
 
 public class items {
-    public static final Item ADAMANTIUM_HELMET = register(
-            "adamantium_helmet",
-            new Item.Settings().armor(adamantium.ADAMANTIUM_ARMORMATERIAL, EquipmentType.HELMET),
-            Item::new
-    );
-    public static final Item ADAMANTIUM_CHESTPLATE = register(
-            "adamantium_chestplate",
-            new Item.Settings().armor(adamantium.ADAMANTIUM_ARMORMATERIAL,EquipmentType.CHESTPLATE),
-            Item::new
-    );
-
-    public static final Item ADAMANTIUM_LEGGINGS = register(
-            "adamantium_leggings",
-            new Item.Settings().armor(adamantium.ADAMANTIUM_ARMORMATERIAL,EquipmentType.LEGGINGS),
-            Item::new
-    );
-
-    public static final Item ADAMANTIUM_BOOTS = register(
-            "adamantium_boots",
-            new Item.Settings().armor(adamantium.ADAMANTIUM_ARMORMATERIAL,EquipmentType.BOOTS),
-            Item::new
-    );
+//    public static final Item ADAMANTIUM_HELMET = register(
+//            "adamantium_helmet",
+//            new Item.Settings().armor(adamantium.ADAMANTIUM_ARMORMATERIAL, EquipmentType.HELMET),
+//            Item::new
+//    );
+//    public static final Item ADAMANTIUM_CHESTPLATE = register(
+//            "adamantium_chestplate",
+//            new Item.Settings().armor(adamantium.ADAMANTIUM_ARMORMATERIAL,EquipmentType.CHESTPLATE),
+//            Item::new
+//    );
+//
+//    public static final Item ADAMANTIUM_LEGGINGS = register(
+//            "adamantium_leggings",
+//            new Item.Settings().armor(adamantium.ADAMANTIUM_ARMORMATERIAL,EquipmentType.LEGGINGS),
+//            Item::new
+//    );
+//
+//    public static final Item ADAMANTIUM_BOOTS = register(
+//            "adamantium_boots",
+//            new Item.Settings().armor(adamantium.ADAMANTIUM_ARMORMATERIAL,EquipmentType.BOOTS),
+//            Item::new
+//    );
+    public static final Item ADAMANTIUM_CHAINMAIL_HELMET = register("cs", new Item.Settings(), Item::new);
     private static Item register(String name, Item.Settings settings, Function<Item.Settings, Item> itemFactory) {
         RegistryKey<Item> itemKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(at_mite.MOD_ID, name));
         Item item = itemFactory.apply(settings.registryKey(itemKey));
