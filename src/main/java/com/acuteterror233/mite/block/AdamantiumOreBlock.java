2734dbb408a7270b0a1fdf58754b4da22754b672
zxcmnb <551775262@qq.com>
2025-06-17 00:29:44 +0800

    6.17

--- src/main/java/com/acuteterror233/mite/block/AdamantiumOreBlock.java
+++ src/main/java/com/acuteterror233/mite/block/AdamantiumOreBlock.java
@@ -1,43 +1,27 @@
 package com.acuteterror233.mite.block;
 
-import com.mojang.serialization.MapCodec;
+
 import net.minecraft.block.BlockState;
-import net.minecraft.block.BlockWithEntity;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.block.CraftingTableBlock;
+import net.minecraft.screen.CraftingScreenHandler;
+import net.minecraft.screen.NamedScreenHandlerFactory;
+import net.minecraft.screen.ScreenHandlerContext;
+import net.minecraft.screen.SimpleNamedScreenHandlerFactory;
 import net.minecraft.text.Text;
-import net.minecraft.util.ActionResult;
-import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 
+public class AdamantiumOreBlock extends CraftingTableBlock {
+    private static final Text TITLE = Text.translatable("container.crafting");
 
-
-public class AdamantiumOreBlock extends BlockWithEntity {
-
-    protected AdamantiumOreBlock(Settings settings) {
+    public AdamantiumOreBlock(Settings settings) {
         super(settings);
     }
 
     @Override
-    protected MapCodec<? extends BlockWithEntity> getCodec() {
-        return createCodec(AdamantiumOreBlock::new);
+    protected NamedScreenHandlerFactory createScreenHandlerFactory(BlockState state, World world, BlockPos pos) {
+        return new SimpleNamedScreenHandlerFactory(
+                (syncId, inventory, player) -> new CraftingScreenHandler(syncId, inventory, ScreenHandlerContext.create(world, pos)), TITLE
+        );
     }
-
-    @Override
-    public @Nullable BlockEntity createBlockEntity(BlockPos pos, BlockState state) {
-        return new AdamantiumOreBlockEntity(pos, state);
-    }
-    @Override
-    protected ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
-        if (!(world.getBlockEntity(pos) instanceof AdamantiumOreBlockEntity BlockEntity)) {
-            return super.onUse(state, world, pos, player, hit);
-        }
-        BlockEntity.incrementClicks();
-        player.sendMessage(Text.of("You've clicked the block for the " + BlockEntity.getClicks() + "th time."),true);
-
-        return ActionResult.SUCCESS;
-    }
-
 }
--- src/main/java/com/acuteterror233/mite/block/At_Blocks.java
+++ src/main/java/com/acuteterror233/mite/block/At_Blocks.java
@@ -1,13 +1,8 @@
 package com.acuteterror233.mite.block;
 
 import com.acuteterror233.mite.At_mite;
-import net.fabricmc.fabric.api.object.builder.v1.block.entity.FabricBlockEntityTypeBuilder;
 import net.minecraft.block.*;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.item.Items;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
@@ -20,16 +15,6 @@ public class At_Blocks {
             AdamantiumOreBlock::new,
             AbstractBlock.Settings.create());
 
-    public static final BlockEntityType<AdamantiumOreBlockEntity> ADAMANTIUM_ORE_BLOCK_ENTITY =
-            register("adamantium_ore_block_entity", AdamantiumOreBlockEntity::new, ADAMANTIUM_ORE);
-
-    private static <T extends BlockEntity> BlockEntityType<T> register(
-            String name,
-            FabricBlockEntityTypeBuilder.Factory<? extends T> entityFactory,
-            Block... blocks
-    ) {
-        return Registry.register(Registries.BLOCK_ENTITY_TYPE, Identifier.of(At_mite.MOD_ID, name), FabricBlockEntityTypeBuilder.<T>create(entityFactory, blocks).build());
-    }
     private static Block register(String name, Function<AbstractBlock.Settings, Block> factory, AbstractBlock.Settings settings) {
         Block block = Blocks.register(RegistryKey.of(RegistryKeys.BLOCK, Identifier.of(At_mite.MOD_ID, name)), factory, settings);
         Items.register(block);
